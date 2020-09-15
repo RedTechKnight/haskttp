d82b8dbf749deb94c01aa9d54220bec7ec76ad30
RedTechKnight <bloodtechknight@protonmail.com>
2020-09-15 22:04:29 +0200

    Chapter 6: Server is now based on a monad transformer stack.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings,LambdaCase,ViewPatterns #-}
+{-# LANGUAGE OverloadedStrings,LambdaCase,ViewPatterns,GeneralizedNewtypeDeriving #-}
 module Main where
 import Prelude hiding (getContents)
 import Network.Socket
@@ -11,31 +11,44 @@ import System.IO.Error
 import Data.Foldable
 import Data.Bifunctor
 import Data.Tuple
+import Data.Int
 import Control.Concurrent
 import Control.Applicative
+import Control.Arrow
 import Control.Monad
+import Control.Monad.Reader
+import Control.Monad.State.Lazy
 main :: IO ()
 main = do
   sock <- socket AF_INET Stream defaultProtocol
   bind sock (SockAddrInet 4510 (tupleToHostAddress (127,0,0,1)))
   listen sock 1
   (conn,addr) <- accept sock
-  runDictServer conn Map.empty
+  flip runStateT Map.empty . flip runReaderT conn . unwrapDictServer $ runDictServer
   close sock
 
-runDictServer :: Socket -> Map.Map B.ByteString B.ByteString -> IO ()
-runDictServer clientConn map = second (C.dropWhile (==' ')) . C.break (==' ') . C.takeWhile (/='\n') <$> recv clientConn 4096 >>= \case
-  ("GET",key) -> case Map.lookup (C.takeWhile (/=' ') $ key) map of
-    Nothing -> send clientConn (fold ["ERROR key ",key,",not found in dictionary.\n"]) >> runDictServer clientConn map
-    Just value -> send clientConn (fold ["ANSWER ",value,"\n"]) >> runDictServer clientConn map
-  ("SET",args) -> let key = C.takeWhile (/=' ') args
-                      value = C.takeWhile (/= '\n') . C.drop 1 . C.dropWhile (/=' ') $ args
-                  in case (key,value) of
-                       ("",_) -> send clientConn "ERROR No key specified.\n" >> runDictServer clientConn map
-                       (_,"") -> send clientConn "ERROR No value specified.\n" >> runDictServer clientConn map
-                       (key,value) -> send clientConn (fold ["SUCCESS Set key ",key," to value ",value,"\n"]) >> runDictServer clientConn (Map.insert key value map)
-  ("CLEAR",_) -> send clientConn "SUCCESS clearing all contents." >> runDictServer clientConn Map.empty
-  ("ALL",_) -> let kvs = foldMap (\(k,v) -> k <> ": " <> v <> "\n") $ Map.toList map in send clientConn ("ANSWER Presenting definitions of all words stored:\n" <> kvs) >> runDictServer clientConn map
-  ("EXIT",_) -> send clientConn "FINISHED" >> close clientConn
-  (comm,_) -> send clientConn (fold ["ERROR Unrecognised command ",comm," received. Ignoring.\n"]) >> runDictServer clientConn map
- 
+type Dictionary = Map.Map B.ByteString B.ByteString
+newtype DictServer a = DictServer {unwrapDictServer :: ReaderT Socket (StateT Dictionary IO) a}
+  deriving (Functor,Applicative,Monad,MonadReader Socket,MonadState Dictionary,MonadIO)
+
+runDictServer :: DictServer ()
+runDictServer = ask >>= fmap spanOnSpace . (liftIO . flip recv 4096) >>= uncurry handleMessage
+
+handleMessage :: B.ByteString -> B.ByteString -> DictServer ()
+handleMessage "GET" (C.takeWhile (/=' ') -> key) = gets (Map.lookup key) >>= \case
+  Nothing -> respond (fold ["ERROR key ",key," not found in dictionary.\n"]) >> runDictServer
+  Just value -> respond (fold ["ANSWER ",value,"\n"]) >> runDictServer
+handleMessage "SET" (spanOnSpace -> ("",_)) = respond "ERROR First parameter is empty.\n" >> runDictServer
+handleMessage "SET" (spanOnSpace -> (_,"")) = respond "ERROR Second parameter is empty.\n" >> runDictServer
+handleMessage "SET" (spanOnSpace -> (key,val)) = modify (Map.insert key val) >> respond "SUCCESS Key inserted successfully.\n" >> runDictServer
+handleMessage "CLEAR" _ = modify (const Map.empty) >> respond "SUCCESS All dictionary entries erased.\n" >> runDictServer
+handleMessage "REMOVE" (C.takeWhile (/=' ') -> key) = modify (Map.delete key) >> respond "SUCCESS Matching key removed from dictionary.\n" >> runDictServer
+handleMessage "ALL" _ =  gets (B.toLazyByteString . (B.lazyByteString "ANSWER Displaying all entries in dictionary:\n" <>) . foldMap (\(k,v) -> B.lazyByteString k <> ": " <> B.lazyByteString v <> "\n") . Map.toList) >>= respond >> runDictServer
+handleMessage "EXIT" _ = respond "ENDED" >> pure ()
+handleMessage comm _ = respond (fold ["ERROR Unrecognised command ",comm," received.\n"]) >> runDictServer
+
+respond :: B.ByteString -> DictServer Int64
+respond msg = ask >>= (liftIO . flip send msg) 
+
+spanOnSpace :: B.ByteString -> (B.ByteString,B.ByteString)
+spanOnSpace = (C.takeWhile (/=' ') &&& C.drop 1 . C.dropWhile (/=' ')) . C.takeWhile (/='\n')
