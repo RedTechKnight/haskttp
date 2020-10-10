54066da84fa62b4942f09723e069f0cfe8c0e518
RedTechKnight <bloodtechknight@protonmail.com>
2020-10-10 15:07:52 +0200

    Format handleMessage functions, make DictServer a monad transformer and make necessary changes due to that.

--- src/Server.hs
+++ src/Server.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE OverloadedStrings,LambdaCase,ViewPatterns,GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings,LambdaCase,ViewPatterns,GeneralizedNewtypeDeriving,MultiParamTypeClasses,TypeFamilies,FlexibleInstances,FlexibleContexts #-}
 module Server where
-import Network.Socket
+import Network.Socket hiding (listen)
 import Network.Socket.ByteString.Lazy
 import Data.ByteString.Lazy (ByteString(..))
 import qualified Data.ByteString.Lazy.Char8 as C
@@ -12,34 +12,73 @@ import Data.Foldable
 import Data.Int
 import Control.Monad.Reader
 import Control.Monad.State.Lazy
+import Control.Monad.Writer.Lazy as W hiding (All)
+import Control.Monad.Trans.Control
+import Control.Monad.Identity
+import Control.Monad.Base
 import Request.Parser
 import Hectoparsec
+import Control.Concurrent.MVar
+import Control.Concurrent.Async.Lifted
+import Control.Arrow
 
 
 type Dictionary = Map.Map ByteString ByteString
-newtype DictServer a = DictServer {unwrapDictServer :: ReaderT Socket (StateT Dictionary IO) a}
-  deriving (Functor,Applicative,Monad,MonadReader Socket,MonadState Dictionary,MonadIO)
+newtype DictServer m a = DictServer {unwrapDictServer :: ReaderT (Socket,MVar Dictionary) m a}
+  deriving (Functor,Applicative,Monad,MonadReader (Socket,MVar Dictionary),MonadTrans)  
+instance MonadIO (DictServer IO) where
+  liftIO action = DictServer (ReaderT $ \r -> action)
+  
+instance MonadBase IO (DictServer IO) where
+  liftBase = liftBaseDefault
 
-runDictServer :: DictServer ()
-runDictServer = ask
+instance MonadTransControl DictServer where
+  type StT DictServer a = a
+  liftWith f = DictServer (ReaderT (\r -> f (\action -> runReaderT (unwrapDictServer action) r)))
+  restoreT action = DictServer (ReaderT (const action))
+
+instance MonadBaseControl IO (DictServer IO) where
+  type StM (DictServer IO) a = a
+  liftBaseWith f = DictServer (ReaderT (\r -> f (flip runReaderT r . unwrapDictServer)))
+  restoreM = DictServer . return
+
+runDictServer :: DictServer IO ()
+runDictServer  = asks fst
   >>= liftIO . accept
-  >>= \(conn,_) -> (fmap parseRequest . (liftIO . flip recv 4096) $ conn)
+  >>= \(conn,_) -> concurrently_ (local (const conn *** id) serveClient) runDictServer
+  
+serveClient :: DictServer IO ()
+serveClient = liftIO (print "Serving client...")
+  >> asks fst
+  >>= fmap parseRequest . (liftIO . flip recv 4096)
   >>= \case
-    Right req -> local (const conn) (handleMessage req) >> liftIO (close conn) >> case req of
-      Exit -> pure ()
-      _ -> runDictServer
-    Left err -> local (const conn) (respond (C.pack $ show (parseErrorItem err))) >> liftIO (close conn) >> runDictServer
+  Right req -> handleMessage req >> case req of
+    Exit -> pure ()
+    _ -> serveClient
+  Left err -> respond (C.pack $ show (parseErrorItem err)) >> serveClient
 
-handleMessage :: Request -> DictServer ()
-handleMessage (Get key) = gets (Map.lookup key) >>= \case
+handleMessage :: Request -> DictServer IO ()
+handleMessage (Get key) = asks snd >>= liftIO . (flip withMVar (return . Map.lookup key)) >>= \case
   Nothing -> respond (fold ["ERROR key ",key," not found in dictionary.\n"]) >> pure ()
   Just value -> respond (fold ["ANSWER ",value,"\n"]) >> pure ()
-handleMessage (Set key val) = modify (Map.insert key val) >> respond "SUCCESS Key inserted successfully.\n" >> pure ()
-handleMessage Clear = modify (const Map.empty) >> respond "SUCCESS All dictionary entries erased.\n" >> pure ()
-handleMessage (Remove key) = modify (Map.delete key) >> respond "SUCCESS Matching key removed from dictionary.\n" >> pure ()
-handleMessage All =  gets (B.toLazyByteString . (B.lazyByteString "ANSWER Displaying all entries in dictionary:\n" <>) . foldMap (\(k,v) -> B.lazyByteString k <> ": " <> B.lazyByteString v <> "\n") . Map.toList) >>= respond >> pure ()
-handleMessage Exit = respond "ENDED\n" >> pure ()
+handleMessage (Set key val) = asks snd
+  >>= liftIO . (flip modifyMVar_ (return . Map.insert key val))
+  >> respond "SUCCESS Key inserted successfully.\n"
+  >> pure ()
+handleMessage Clear = asks snd
+  >>= liftIO . (flip modifyMVar_ (return . const Map.empty))
+  >> respond "SUCCESS All dictionary entries erased.\n"
+  >> pure ()
+handleMessage (Remove key) = asks snd
+  >>= liftIO . (flip modifyMVar_ (return . (Map.delete key)))
+  >> respond "SUCCESS Matching key removed from dictionary.\n"
+  >> pure ()
+handleMessage All = asks snd
+  >>= liftIO . (flip withMVar (return . (B.toLazyByteString . (B.lazyByteString "ANSWER Displaying all entries in dictionary:\n" <>) . foldMap (\(k,v) -> B.lazyByteString k <> ": " <> B.lazyByteString v <> "\n")) . Map.toList))
+  >>= respond
+  >> pure ()
+handleMessage Exit = respond "ENDED\n" >> asks fst >>= liftIO . close >> pure ()
 
-respond :: B.ByteString -> DictServer Int64
-respond msg = ask >>= (liftIO . flip send msg) 
+respond :: B.ByteString -> DictServer IO Int64
+respond msg = asks fst >>= (liftIO . flip send msg) 
 
